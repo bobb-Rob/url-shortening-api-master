42880f3aed82f82b23eaeb46352a3436b7ea439c
Robertson Arthur <robertsonakpan@gmail.com>
2022-06-24 23:36:19 +0100

    setup folder with webpack

---
+++ dist/main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdXJsLXNob3J0ZW5pbmctYXBpLW1hc3Rlci8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJcclxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
