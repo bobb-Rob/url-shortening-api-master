660adfa512a670ffecdfc334ed529e88af895215
Robertson Arthur <robertsonakpan@gmail.com>
2022-06-30 15:46:29 +0100

    finish mobile design

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 
+import logoBottom from '../images/logo-bottom.svg';
 import logo from '../images/logo.svg';
 import illustrationImg from '../images/illustration-working.svg';
 import hamburger from '../images/menu-icon.svg';
 import iconBrandRecog from '../images/icon-brand-recognition.svg';
 import iconDetailedRecords from '../images/icon-detailed-records.svg';
 import iconFullyCustomizable from '../images/icon-fully-customizable.svg';
+import bgBoostMobile from '../images/bg-boost-mobile.svg';
 import './style/style.css';
 import './style/headline.css';
 import './style/statistic.css';
 
 const logoEl = document.querySelector('.logo');
 logoEl.src = logo;
+
+const bottomLogoEl = document.querySelector('.logo-bottom');
+bottomLogoEl.src = logoBottom;
+
 const hamburgerEl = document.querySelector('.hamburger');
 hamburgerEl.src = hamburger;
 
@@ -22,3 +28,6 @@ const [icon1, icon2, icon3 ] = allStatisticsIcons;
 icon1.src = iconBrandRecog; 
 icon2.src = iconDetailedRecords;
 icon3.src = iconFullyCustomizable;
+
+const boostSection = document.querySelector('.boost-link-section');
+bgBoostMobile
